2656256ccb118d5b1c95e80a17d70340879a5625
dianadutka <diana.dutka@gmail.com>
2024-11-12 20:50:46 +0100

    add age

---
+++ src/main/age/Main.java
@@ -0,0 +1,12 @@
+package main.age;
+
+
+// TODO 1: Ask a user to enter their age in the terminal, check if they are older than 18, if yes write "come in", otherwise "denied"
+// TODO 2: Improve by asking again and again, until the user enters the age above 18
+// TODO 3: what happens if the user enters a text "abc"? Can you improve the code?
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
