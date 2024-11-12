3da7fe69744701a3dad553bd8bee70f9d5941086
dianadutka <diana.dutka@gmail.com>
2024-11-12 21:15:13 +0100

    types

---
+++ src/main/types/Main.java
@@ -0,0 +1,26 @@
+package main.types;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO: what is the difference? Provide type for each variable.
+        var example1 = "hot dogs";
+        var example2 = 'd';
+
+        // TODO: what is the difference? Provide type for each variable.
+        var example3 = "true";
+        var example4 = true;
+
+        // TODO: what is the difference? Provide type for each variable.
+        var example5 = 149;
+        var example6 = 22L;
+
+        // TODO: what is the difference? Provide type for each variable.
+        var example7 = 0.23;
+        var example8 = 0.5F;
+
+        // examples
+        int theLongestInt = 2_147_483_647;
+        long theLong = 2_147_483_648L;
+    }
+}
